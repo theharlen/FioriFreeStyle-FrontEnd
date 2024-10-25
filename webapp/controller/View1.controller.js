af154480867dfb6f4431745a131edcf116c122ce
theharlen <32777370+theharlen@users.noreply.github.com>
2024-10-25 12:28:55 -0300

    Dialog

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -6,13 +6,35 @@ function (Controller, MessageToast) {
     "use strict";
 
     return Controller.extend("zso.controller.View1", {
-
         onInit: function () {
-
         },
 
-        onHeader: function(){
-            alert("onHeader");
+        onOpenDialogInfo() {
+            var oController  = this;
+            //saving view fragment name
+            var sName = "zso.view.DialogInfo";
+            
+            if(!this.oDialog){
+                /*
+                this.loadFragment({
+                    //view fragment
+                    name: sName
+                }).then(function(oDialog2) {
+                    oController.oDialog = oDialog2;
+                    oController.oDialog.open();
+                }.bind(this));
+                */
+
+                this.oDialog = this.byId("DialogInfo");
+                oController.oDialog.open();
+            }else{
+                this.oDialog.open();
+            }
+        },
+        
+        onCloseDialogInfo: function(){
+            //this.byId("DialogInfo").close();
+            this.oDialog.close();
         }
     });
 });
