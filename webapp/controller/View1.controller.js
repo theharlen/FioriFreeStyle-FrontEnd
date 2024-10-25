340445b50a3021f84274cdc3ed764c09151b2def
theharlen <32777370+theharlen@users.noreply.github.com>
2024-10-25 12:48:12 -0300

    Batch and normal Request

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -9,32 +9,53 @@ function (Controller, MessageToast) {
         onInit: function () {
         },
 
-        onOpenDialogInfo() {
-            var oController  = this;
-            //saving view fragment name
-            var sName = "zso.view.DialogInfo";
-            
-            if(!this.oDialog){
-                /*
-                this.loadFragment({
-                    //view fragment
-                    name: sName
-                }).then(function(oDialog2) {
-                    oController.oDialog = oDialog2;
-                    oController.oDialog.open();
-                }.bind(this));
-                */
-
-                this.oDialog = this.byId("DialogInfo");
-                oController.oDialog.open();
-            }else{
-                this.oDialog.open();
-            }
+        onNormalRequest() {
+            this.executeRequests(false);
         },
-        
-        onCloseDialogInfo: function(){
-            //this.byId("DialogInfo").close();
-            this.oDialog.close();
+
+        onBatchRequest() {
+            this.executeRequests(true);
+        },
+
+        executeRequests(bUseBatch) {
+            var oModel = this.getOwnerComponent().getModel();
+
+            //Setting server call (normal or batch)
+            oModel.setUseBatch(bUseBatch);
+            
+            console.log("Executing requests");
+            console.log("-------------------------------------------");
+
+            // requisição 1
+            oModel.read("/SoHeaderSet('1')",{
+                success: function(oData2, oResponse){
+                    console.log("Reading key 1: OK");
+                },
+                error: function(oError){
+                    console.log("Reading key 1: Erro");
+                    console.log(oError);
+                }
+            });
+
+            // requisição 2
+            oModel.read("/SoHeaderSet('2')",{
+                success: function(oData2, oResponse){
+                    console.log("Reading key 2: OK");
+                },
+                error: function(oError){
+                    console.log("Reading key 2: Erro");
+                }
+            });
+
+            // requisição 3
+            oModel.remove("/SoHeaderSet('3')",{
+                success: function(oData2, oResponse){
+                    console.log("Exclusão chave 3: OK");
+                },
+                error: function(oError){
+                    console.log("Exclusão chave 3: Erro");
+                }
+            });
         }
     });
 });
