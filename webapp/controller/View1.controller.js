552d6e088336b8937453acac53fac678a48b01e4
theharlen <32777370+theharlen@users.noreply.github.com>
2024-10-29 08:40:38 -0300

    Debug

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -9,79 +9,29 @@ function (Controller, MessageToast) {
         onInit: function () {
         },
 
-        onTestCallback() {
-            console.log("[Process 1] Start");
-            this.onGetOrder(1,function(oData,oResponse){
-                console.log("[Process 2] call finished");
-                console.log(oData);
-                console.log(oResponse);
-            });
-            console.log("[Process 1] End");
-        },
-
-        onGetOrder: function(iId,callback){
-            var oModel = this.getOwnerComponent().getModel();
-            oModel.setUseBatch(false);
-
-            oModel.read("/SoHeaderSet('"+iId+"')",{
+        onShowTotalValue: function(){
+            //debugger;
+            var that     = this;
+            var sOrderId = this.getView().byId("OrderId").getValue();
+            var oModel   = this.getOwnerComponent().getModel();
+            
+            oModel.read("/SoHeaderSet('"+sOrderId+"')",{
                 success: function(oData2, oResponse){
-                    callback(oData2,oResponse);
+                    var sMensagem = that.mountMessage(oData2);
+                    alert(sMensagem);
                 },
                 error: function(oError){
                 }
             });
         },
 
-        onTestPromise1() {
-            console.log("[Process 1] Start");
-            var oPromise1 = this.onGetOrder2(1);
-            oPromise1.then(function(oData,oResponse){
-                console.log("[Process 2] Finished");
-                console.log(oData);
-                console.log(oResponse);
-            });
-            console.log("[Process 1] End");
-        },
+        mountMessage: function(oOrder){
+            var sMensagem = "";
 
-        onTestPromise2() {
-            console.log("[Process 1] Start");
-
-            var oPromise1 = this.onGetOrder2(1);
-            oPromise1.then(function(oData,oResponse){
-                console.log("[Process 2] Finished");
-                //console.log(oData);
-                //console.log(oResponse);
-            });
-
-            var oPromise2 = this.onGetOrder2(2);
-            oPromise2.then(function(oData,oResponse){
-                console.log("[Process 3] Finished");
-                //console.log(oData);
-                //console.log(oResponse);
-            });
+            sMensagem += "The total value: ";
+            sMensagem += oOrder.NetValue;
             
-            Promise.all([oPromise1,oPromise2]).then(function(aRetorno){
-                console.log("[Process 4] Promise.all Finished");
-                console.log(aRetorno);
-            });
-
-            console.log("[Process 1] End");
-        },
-
-        onGetOrder2: function(iId){
-            var oModel = this.getOwnerComponent().getModel();
-            oModel.setUseBatch(false);
-
-            return new Promise(function(resolve,reject){
-                oModel.read("/SoHeaderSet('"+iId+"')",{
-                    success: function(oData2, oResponse){
-                        resolve({oData: oData2, oResponse: oResponse});
-                    },
-                    error: function(oError){
-                        reject({oError:oError});
-                    }
-                });
-            });
+            return sMensagem;
         }
     });
 });
